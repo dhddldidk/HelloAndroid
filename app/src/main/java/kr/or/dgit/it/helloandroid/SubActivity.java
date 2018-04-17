9a0e90eb26b494fba7c5eb82df271e18a56b3b4e
HyeSeon Yang <skcm19@naver.com>
2018-04-17 11:07:56 +0900

    SubActivity에서 버튼텍스트 수정

--- app/src/main/java/kr/or/dgit/it/helloandroid/SubActivity.java
+++ app/src/main/java/kr/or/dgit/it/helloandroid/SubActivity.java
@@ -13,7 +13,7 @@ public class SubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle(R.string.subavtivity);
         Button btnClose = new Button(getApplicationContext());
-        btnClose.setText("종료 누르세요");
+        btnClose.setText("종료버튼을 누르세요 병합");
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
